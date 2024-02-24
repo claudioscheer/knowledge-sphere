bbf305e5b1b649a49ee00fe78b6687d9863a2a49
Claudio Scheer <claudioscheer@protonmail.com>
2024-02-24 12:36:19 -0300

    feat: add embedding api

---
+++ ks-embedding-api/README.md
@@ -0,0 +1,4 @@
+## How to run
+
+- Install the dependencies;
+- Run `make run` to start the server locally;
---
+++ ks-embedding-api/src/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from embed import get_text_embeddings
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return jsonify({"name": "ks-embedding-api"})
+
+
+@app.route("/embedding", methods=["POST"])
+def embedding():
+    data = request.get_json()
+    text_list = data.get("text", [])
+    if len(text_list) == 0:
+        return jsonify({"error": "no text provided"}), 400
+
+    embeddings = get_text_embeddings(text_list)
+    return jsonify({"embeddings": embeddings})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
